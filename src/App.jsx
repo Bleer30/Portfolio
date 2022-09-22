6007addf0a723b850f211956f2bcc9499687527e
bleer <stif2001@gmail.com>
2022-09-22 13:31:24 -0700

    fix: bad header call

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './header'
-import Profile from './Profile'
-import Resume from './Resume'
-import Contact from './Contact'
+import { useState } from 'react';
+import './App.css';
+import Header from './Header';
+import Profile from './Profile';
+import Resume from './Resume';
+import Contact from './Contact';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <Header/>
-      <Profile/>
-      <Resume/>
-      <Contact/>
+      <Header />
+      <Profile />
+      <Resume />
+      <Contact />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
