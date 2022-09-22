f33222960f02634f948e40b7458750ca0c9904d3
bleer <stif2001@gmail.com>
2022-09-22 12:54:20 -0700

    update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,10 @@
-import './Header.css'
-
 const Header = () => {
-    return ( 
-        <div>
-         <h2>Bleer Rodriguez</h2>
-         <p>Developer</p>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <h2>Bleer Rodriguez</h2>
+      <p>Developer</p>
+    </div>
+  );
+};
+
 export default Header;
