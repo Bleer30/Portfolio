e571ebda89061cf7369d28ac1a389ed267514623
bleer <stif2001@gmail.com>
2022-09-23 08:58:50 -0700

    update header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,60 @@
+import React from 'react';
+import './header.css';
+
 const Header = () => {
   return (
-    <div>
-      <h2>Bleer Rodriguez B.</h2>
-      <p>Developer</p>
-    </div>
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Bleer Rodriguez B.
+        </a>
+
+        <div className="nav__menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="uil uil-home uil__icon"></i> Home
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user uil__icon"></i> About
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#skillis" className="nav__link">
+                <i className="uil uil-file-alt uil__icon"></i> Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt uil__icon"></i> Services
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#portafolio" className="nav__link">
+                <i className="uil uil-scenery uil__icon"></i> Portfolio
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message uil__icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+
+          <i className="uil uil-times nav-close"></i>
+        </div>
+
+        <div className="nav-toggle">
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
   );
 };
 
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,18 +1,24 @@
 const Profile = () => {
-    return ( 
-        <div>
-            <br/>
-            <h2>About Me:</h2>
-            <p>I'm Bleer Rodriguez, a web designer and desktop application developer in Costa Rica</p>
-            <p>Age 21</p>
-            <p>Residence CR</p>
-            <p>Address San Jose,CR</p>
-            <br/>
-            <h2>My Services:</h2>
-            <p>Web development:</p>
-            <p>Desktop application developer</p>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <h2>About Me:</h2>
+      <p>
+        I'm Bleer Rodriguez, a web designer and desktop application developer in
+        Costa Rica
+      </p>
+      <p>Age 21</p>
+      <p>Residence CR</p>
+      <p>Address San Jose,CR</p>
+      <br />
+      <h2>My Services:</h2>
+      <p>Web development:</p>
+      <p>Desktop application developer</p>
+    </div>
+  );
+};
+
 export default Profile;
