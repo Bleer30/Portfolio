381aa0c90cc9db8d5a4b088f68854e749bd20500
bleer <stif2001@gmail.com>
2022-09-29 14:01:19 -0700

    Add styles to the skillis component

--- src/componets/Home/Data.jsx
+++ src/componets/Home/Data.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Data = () => {
   return (
     <div className="home-data">
-      <h1 className="home-title">Bleer Rodriguez🤙</h1>
+      <h1 className="home-title">Bleer Rodriguez 🖐</h1>
       <h3 className="home-subtitle">Developer</h3>
       <p className="home-description">
         I am a web developer who is passionate
