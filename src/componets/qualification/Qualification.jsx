5bf8f7117e835670ee4144391c3a24f579489661
bleer <stif2001@gmail.com>
2022-10-05 17:08:14 -0700

    add functionality to titles and change styles

--- src/componets/qualification/Qualification.jsx
+++ src/componets/qualification/Qualification.jsx
@@ -21,23 +21,37 @@ const Qualification = () => {
                 ? 'qualification-button qualification-active button--flex'
                 : 'qualification-button button--flex'
             }
+            onClick={() => toggleTab(1)}
           >
             <i className="uil uil-graduation-cap qualification-icon"></i>{' '}
             Education
           </div>
 
-          <div className="qualification-button button--flex">
+          <div
+            className={
+              toggleState === 2
+                ? 'qualification-button qualification-active button--flex'
+                : 'qualification-button button--flex'
+            }
+            onClick={() => toggleTab(2)}
+          >
             <i className="uil uil-briefcase-alt qualification-icon"></i>{' '}
             Experience
           </div>
         </div>
 
         <div className="qualification-sections">
-          <div className="qualification-content qualification-content-active">
+          <div
+            className={
+              toggleState === 1
+                ? 'qualification-content qualification-content-active'
+                : 'qualification-content'
+            }
+          >
             <div className="qualification-data">
               <div>
                 <h3 className="qualification-title">
-                  Bachelor of systems engineering
+                  Licenciatura of systems engineering
                 </h3>
                 <span className="qualification-subtitle">
                   Universidad Internacional San Isidro Labrador
@@ -69,63 +83,42 @@ const Qualification = () => {
                   Universidad Internacional San Isidro Labrador
                 </span>
                 <div className="qualification-calender">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                  <i className="uil uil-calendar-alt"></i> 2018 - 2021
                 </div>
               </div>
             </div>
 
             <div className="qualification-data">
               <div>
-                <h3 className="qualification-title">
-                  Bachelor of systems engineering
-                </h3>
+                <h3 className="qualification-title">High School degree</h3>
                 <span className="qualification-subtitle">
-                  Universidad Internacional San Isidro Labrador
+                  Liceo Los Angeles Pital
                 </span>
                 <div className="qualification-calender">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                  <i className="uil uil-calendar-alt"></i> 2013 - 2018
                 </div>
               </div>
 
               <div>
                 <span className="qualification-rounder"></span>
                 <span className="qualification-line"></span>
-              </div>
-            </div>
-
-            <div className="qualification-data">
-              <div></div>
-
-              <div>
-                <span className="qualification-rounder"></span>
-                <span className="qualification-line"></span>
-              </div>
-
-              <div>
-                <h3 className="qualification-title">
-                  Bachelor of systems engineering
-                </h3>
-                <span className="qualification-subtitle">
-                  Universidad Internacional San Isidro Labrador
-                </span>
-                <div className="qualification-calender">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
-                </div>
               </div>
             </div>
           </div>
 
-          <div className="qualification-content">
+          <div
+            className={
+              toggleState === 2
+                ? 'qualification-content qualification-content-active'
+                : 'qualification-content'
+            }
+          >
             <div className="qualification-data">
               <div>
-                <h3 className="qualification-title">
-                  Bachelor of systems engineering
-                </h3>
-                <span className="qualification-subtitle">
-                  Universidad Internacional San Isidro Labrador
-                </span>
+                <h3 className="qualification-title">Developer</h3>
+                <span className="qualification-subtitle">SIM</span>
                 <div className="qualification-calender">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
                 </div>
               </div>
 
@@ -144,54 +137,12 @@ const Qualification = () => {
               </div>
 
               <div>
-                <h3 className="qualification-title">
-                  Bachelor of systems engineering
-                </h3>
+                <h3 className="qualification-title">Freelancer</h3>
                 <span className="qualification-subtitle">
-                  Universidad Internacional San Isidro Labrador
+                  to various people who needed my services
                 </span>
                 <div className="qualification-calender">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
-                </div>
-              </div>
-            </div>
-
-            <div className="qualification-data">
-              <div>
-                <h3 className="qualification-title">
-                  Bachelor of systems engineering
-                </h3>
-                <span className="qualification-subtitle">
-                  Universidad Internacional San Isidro Labrador
-                </span>
-                <div className="qualification-calender">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
-                </div>
-              </div>
-
-              <div>
-                <span className="qualification-rounder"></span>
-                <span className="qualification-line"></span>
-              </div>
-            </div>
-
-            <div className="qualification-data">
-              <div></div>
-
-              <div>
-                <span className="qualification-rounder"></span>
-                <span className="qualification-line"></span>
-              </div>
-
-              <div>
-                <h3 className="qualification-title">
-                  Bachelor of systems engineering
-                </h3>
-                <span className="qualification-subtitle">
-                  Universidad Internacional San Isidro Labrador
-                </span>
-                <div className="qualification-calender">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                  <i className="uil uil-calendar-alt"></i> 2021 - 2022
                 </div>
               </div>
             </div>
