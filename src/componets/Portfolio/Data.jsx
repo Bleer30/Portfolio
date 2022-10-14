ace92d3760cbe4eabdaefb303de96c8f7d0bec38
bleer <stif2001@gmail.com>
2022-10-14 14:51:15 -0700

    update image url

--- src/componets/Portfolio/Data.jsx
+++ src/componets/Portfolio/Data.jsx
@@ -1,30 +1,25 @@
-import work1 from '../../assets/work1.jpg';
-import work2 from '../../assets/work2.jpg';
-import work3 from '../../assets/work3.jpg';
-import work4 from '../../assets/work4.jpg';
-
 export const projectsData = [
   {
     id: 1,
-    image: work1,
+    image: '../../assets/work1.jpg',
     title: 'Web designer',
     category: 'web',
   },
   {
     id: 2,
-    image: work2,
+    image: '../../assets/work2.jpg',
     title: 'Web designer',
     category: 'web',
   },
   {
     id: 3,
-    image: work3,
+    image: '../../assets/work3.jpg',
     title: 'Brand desing',
     category: 'desing',
   },
   {
     id: 4,
-    image: work4,
+    image: '../../assets/work4.jpg',
     title: 'App movil',
     category: 'app',
   },
