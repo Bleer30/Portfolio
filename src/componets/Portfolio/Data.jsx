017bfd487e3812e37225f8ee28081b21e1b61cf2
bleer <stif2001@gmail.com>
2022-10-14 15:00:17 -0700

    img

--- src/componets/Portfolio/Data.jsx
+++ src/componets/Portfolio/Data.jsx
@@ -1,25 +1,25 @@
 export const projectsData = [
   {
     id: 1,
-    image: '../../assets/work1.jpg',
+    image: 'work1.jpg',
     title: 'Web designer',
     category: 'web',
   },
   {
     id: 2,
-    image: '../../assets/work2.jpg',
+    image: 'work2.jpg',
     title: 'Web designer',
     category: 'web',
   },
   {
     id: 3,
-    image: '../../assets/work3.jpg',
+    image: 'work3.jpg',
     title: 'Brand desing',
     category: 'desing',
   },
   {
     id: 4,
-    image: '../../assets/work4.jpg',
+    image: 'work4.jpg',
     title: 'App movil',
     category: 'app',
   },
--- src/componets/Portfolio/WorkItems.jsx
+++ src/componets/Portfolio/WorkItems.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const WorkItems = ({ item }) => {
   return (
     <div className="work-card" key={item.id}>
-      <img src={item.image} alt="" className="work-img" />
+      <img src={`./images/${item.image}`} alt="" className="work-img" />
       <h3 className="work-title">{item.name}</h3>
       <a href="#" className="work-button">
         Demo <i className="bx bx-right-arrow-alt work-button-icon"></i>
