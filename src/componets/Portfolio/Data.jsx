8f3f686053829eae86096326987edebfec07f19b
bleer <stif2001@gmail.com>
2022-10-19 08:33:02 -0700

    update portfolio component

--- src/componets/Portfolio/Data.jsx
+++ src/componets/Portfolio/Data.jsx
@@ -4,24 +4,28 @@ export const projectsData = [
     image: 'work1.jpg',
     title: 'Web designer',
     category: 'web',
+    link: 'https://cafe-menu-phi.vercel.app/',
   },
   {
     id: 2,
     image: 'work2.jpg',
     title: 'Web designer',
     category: 'web',
+    link: 'https://day-17-bleer.vercel.app/',
   },
   {
     id: 3,
     image: 'work3.jpg',
     title: 'Brand desing',
     category: 'desing',
+    link: 'https://day-8-bleer.vercel.app/?',
   },
   {
     id: 4,
     image: 'work4.jpg',
     title: 'App movil',
-    category: 'app',
+    category: 'web',
+    link: 'https://day-33-bleer.vercel.app/',
   },
 ];
 
@@ -31,9 +35,6 @@ export const projectsNav = [
   },
   {
     name: 'web',
-  },
-  {
-    name: 'app',
   },
   {
     name: 'desing',
--- src/componets/Portfolio/WorkItems.jsx
+++ src/componets/Portfolio/WorkItems.jsx
@@ -5,7 +5,7 @@ const WorkItems = ({ item }) => {
     <div className="work-card" key={item.id}>
       <img src={`./images/${item.image}`} alt="" className="work-img" />
       <h3 className="work-title">{item.name}</h3>
-      <a href="#" className="work-button">
+      <a href={item.link} className="work-button">
         Demo <i className="bx bx-right-arrow-alt work-button-icon"></i>
       </a>
     </div>
